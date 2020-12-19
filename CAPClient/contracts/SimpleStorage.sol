bef6dd9502aaebdba7e9652e8f32b15c2a2ece4a
Tauburn <jc4sombrero@gmail.com>
2020-12-19 16:17:15 +0100

    initial contract

--- CAPClient/contracts/SimpleStorage.sol
+++ CAPClient/contracts/SimpleStorage.sol
@@ -1,10 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract SimpleStorage {
-  uint storedData;
+  struct patientData{
+    string CIP;
+    uint lastCheck;
+    bool hasCheckedIn;
+    uint CAP;
+    bool exists;
+  }
 
-  event test(address from, uint number);
+  mapping(address => patientData) patients;
+  mapping(string => address) addressFromCIP;
+  mapping(uint => string[]) patientsFromCAP;
+  mapping(uint => uint) activePatientsFromCAP;
+  mapping(address => uint) CAPs;
+
+  uint CAPnumber = 0;
+
+  function registerCAP() external {
+    CAPnumber = CAPnumber+1;
+    CAPs[msg.sender] = CAPnumber;
+  }
+
+  function registerPatient(address patientAddress, string calldata CIP) external{
+    patientData memory data;
+    data.CIP = CIP;
+    data.lastCheck = now;
+    data.CAP = CAPs[msg.sender];
+    data.exists = true;
+    patients[patientAddress] = data;
+    addressFromCIP[CIP] = patientAddress;
+    patientsFromCAP[CAPs[msg.sender]].push(CIP);
+    activePatientsFromCAP[CAPs[msg.sender]] = activePatientsFromCAP[CAPs[msg.sender]]+1;
+  }
+
+  function getAllPatientsFromCAP() external returns( patientData[] memory){
+    patientData[] memory ret = new patientData[](activePatientsFromCAP[CAPs[msg.sender]]);
+    uint i = 0;
+    uint retI = 0;
+    for(i=0; i<activePatientsFromCAP[CAPs[msg.sender]]; ++i){
+      if(patients[addressFromCIP[patientsFromCAP[CAPs[msg.sender]][i]]].exists){
+        ret[retI] = patients[addressFromCIP[patientsFromCAP[CAPs[msg.sender]][i]]];
+        ++retI;
+      }
+    }
+    return ret;
+  }
+
+  /*event test(address from, uint number);
 
   function set(uint x) public {
     storedData = x;
@@ -13,5 +58,5 @@ contract SimpleStorage {
 
   function get() public view returns (uint) {
     return storedData;
-  }
+  }*/
 }
